0ac412657d85f37bc9cd3e08e7f9a3edf05db4df
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-22 22:18:30 +0200

    add root layout, homepage and events page with react router dom

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,44 +1,24 @@
-import { Suspense } from 'react';
-import { useLoaderData, json, defer, Await } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import EventsList from "../components/EventsList";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import app from "../util/firebase";
 
-import EventsList from '../components/EventsList';
+export default function Events() {
+  const [events, setEvents] = useState([]);
+  const db = getFirestore(app);
+  const eventsRef = collection(db, "events");
 
-function EventsPage() {
-  const { events } = useLoaderData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDocs(eventsRef);
+      setEvents(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    fetchData();
+  }, [eventsRef]);
 
   return (
-    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
-      <Await resolve={events}>
-        {(loadedEvents) => <EventsList events={loadedEvents} />}
-      </Await>
-    </Suspense>
+    <>
+      <EventsList events={events} />
+    </>
   );
-}
-
-export default EventsPage;
-
-async function loadEvents() {
-  const response = await fetch('http://localhost:8080/events');
-
-  if (!response.ok) {
-    // return { isError: true, message: 'Could not fetch events.' };
-    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-    //   status: 500,
-    // });
-    throw json(
-      { message: 'Could not fetch events.' },
-      {
-        status: 500,
-      }
-    );
-  } else {
-    const resData = await response.json();
-    return resData.events;
-  }
-}
-
-export function loader() {
-  return defer({
-    events: loadEvents(),
-  });
 }
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,31 +1,8 @@
-import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
-import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-  const token = useLoaderData();
-  const submit = useSubmit();
-
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
-
-    if (token === "EXPIRED") {
-      submit(null, { action: "/logout", method: "post" });
-      return;
-    }
-
-    const tokenDuration = getTokenDuration();
-    console.log(tokenDuration);
-
-    setTimeout(() => {
-      submit(null, { action: "/logout", method: "post" });
-    }, tokenDuration);
-  }, [token, submit]);
-
   return (
     <>
       <MainNavigation />
