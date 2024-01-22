be181c37a9a010205a8174676ac4854c3d1a7b4e
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-22 22:30:23 +0200

    add error page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Events from "./pages/Events";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/events", element: <Events /> },
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,7 @@
 import { useRouteError } from "react-router-dom";
 
 import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
 
 function ErrorPage() {
   const error = useRouteError();
@@ -19,6 +20,7 @@ function ErrorPage() {
 
   return (
     <>
+      <MainNavigation />
       <PageContent title={title}>
         <p>{message}</p>
       </PageContent>
