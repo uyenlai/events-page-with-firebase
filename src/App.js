f34270c7c411c2f69c1d86b84ce978065673e220
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-22 23:08:26 +0200

    add event detail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import Events from "./pages/Events";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import EventDetailPage from "./pages/EventDetail";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/events", element: <Events /> },
+      { path: "/events/:id", element: <EventDetailPage /> },
     ],
   },
 ]);
