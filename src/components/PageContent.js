51ba3ebf375a69ce8ef7f57307e020f918a36e3b
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-24 17:22:18 +0200

    add background

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -2,11 +2,9 @@ import classes from "./PageContent.module.css";
 
 function PageContent({ title, children }) {
   return (
-    <div className={classes.bg}>
-      <div className={classes.content}>
-        <h1>{title}</h1>
-        {children}
-      </div>
+    <div className={classes.content}>
+      <h1>{title}</h1>
+      {children}
     </div>
   );
 }
